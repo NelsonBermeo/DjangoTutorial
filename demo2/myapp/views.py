c5e5547ccd317ab5f6d2d8b8cafdae424e33a6f2
Nelson Bermeo <141251675+Melson12@users.noreply.github.com>
2024-07-17 09:26:11 -0400

    created templates connected urls and views

--- demo2/myapp/views.py
+++ demo2/myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import TodoItem
 # Create your views here.
 
 def home(request):
-    return HttpResponse("hello world!")
+    return render(request, "home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, 'todos.html', {"todos": items})
+
+def about(request):
+    return render(request, "about.html")
